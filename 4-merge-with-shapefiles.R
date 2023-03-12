42fa3932a88b4d224c39551c95849e321b140dcc
Francis DiTraglia <francis.ditraglia@economics.ox.ac.uk>
2023-03-12 15:58:07 +0000

    store cleaned data and re-oragnize scripts accordingly

---
+++ 4-merge-with-shapefiles.R
@@ -0,0 +1,26 @@
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+
+# Get country polygons for all continents excluding Antarctica 
+world <- ne_countries(scale = "medium", 
+                      returnclass = "sf", 
+                      continent = c('North America', 'Africa', 'Europe', 
+                                    'Asia', 'South America', 'Oceania'))
+
+# Presumably these plots are all going to look really similar. We may want to 
+# rethink how we use our remaining figures. Alternatively, we may want do make
+# the plots by continent?
+
+# gu_a3 is geounit iso. Use this to merge bllGBD
+bllWorld <- world |> 
+  left_join(rename(bllGBD, gu_a3 = iso3c))
+
+save(bllWorld, file = './data/bllWorld.RData')
+save(aerosol, file = './data/aerosol.RData')
+save(bllGBD, file = './data/bllGBD.RData')
+
+
+# Clean up
+rm(list = ls())
--- 5-make-plots.R
+++ 5-make-plots.R
@@ -1,68 +1,52 @@
-library(sf)
-library(rnaturalearth)
-library(rnaturalearthdata)
+load('data/bllWorld.RData')
 
 # Set ggplot theme
 theme_set(theme_bw())
 
-# Get country polygons for all continents excluding Antarctica 
-world <- ne_countries(scale = "medium", 
-                      returnclass = "sf", 
-                      continent = c('North America', 'Africa', 'Europe', 
-                                    'Asia', 'South America', 'Oceania'))
-
-# Presumably these plots are all going to look really similar. We may want to 
-# rethink how we use our remaining figures. Alternatively, we may want do make
-# the plots by continent?
-
-# gu_a3 is geounit iso. Use this to merge bllGBD
-world <- world |> 
-  left_join(rename(bllGBD, gu_a3 = iso3c))
-
 # Fraction with BLL > 5 micrograms / deciliter
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = frac5plus)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # Fraction with BLL > 10 micrograms / deciliter
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = frac10plus)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # Average IQ lost per individual aged 0-19: beta approximation
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = beta_IQ_integral)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # $ value of IQ lost per individual aged 0-19: beta approximation
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = beta_returns_no_lead)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # Average IQ lost per individual aged 0-19: lognormal approximation
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = lnorm_IQ_integral)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # $ value of IQ lost per individual aged 0-19: lognormal approximation
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = lnorm_returns_no_lead)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # Average IQ lost per individual aged 0-19: lower bound 
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = LB_IQ_integral)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # $ value of IQ lost per individual aged 0-19: lower bound 
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = LB_returns_no_lead)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
@@ -73,19 +57,20 @@ world |>
 # many countries where lead is a big problem
 
 # Beta 
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = beta_IQ_integral * 1000)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # Lognormal 
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = lnorm_IQ_integral * 1000)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
 
 # Lower bound
-world |> 
+bllWorld |> 
   ggplot() + 
   geom_sf(aes(fill = LB_IQ_integral * 1000)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt")
+
