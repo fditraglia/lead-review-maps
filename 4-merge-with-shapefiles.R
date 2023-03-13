ef0fd21ba93ba5838db9119ce1f5a35f3374d3ae
Francis DiTraglia <francis.ditraglia@economics.ox.ac.uk>
2023-03-13 22:12:44 +0000

    final version of plots and calculation

--- 4-merge-with-shapefiles.R
+++ 4-merge-with-shapefiles.R
@@ -9,10 +9,6 @@ world <- ne_countries(scale = "medium",
                       continent = c('North America', 'Africa', 'Europe', 
                                     'Asia', 'South America', 'Oceania'))
 
-# Presumably these plots are all going to look really similar. We may want to 
-# rethink how we use our remaining figures. Alternatively, we may want do make
-# the plots by continent?
-
 # gu_a3 is geounit iso. Use this to merge bllGBD
 bllWorld <- world |> 
   left_join(rename(bllGBD, gu_a3 = iso3c))
